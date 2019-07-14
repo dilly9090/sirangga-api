2d93af8f7b7018f2f6a731a78662559c165c3df8
ranitachi <fachran.nazarullah@gmail.com>
2019-07-14 17:58:39 +0700

    update

--- config/filesystems.php
+++ config/filesystems.php
@@ -68,7 +68,7 @@ return [
             'username' => 'api',
             'password' => '**api**',
             'port' => 22,
-            'root' => '/home/appserve',
+            'root' => '/home/appserv',
 
             // Settings for SSH key based authentication...
             // 'privateKey' => '/path/to/privateKey',
